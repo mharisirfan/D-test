7c4e0805490c558a1e457d683735aaf4a636ac7c
Muhammad Haris Irfan <harisirfanafzalbutt@gmail.com>
2022-02-06 02:13:49 +0500

    server update

--- server.js
+++ server.js
@@ -6,7 +6,8 @@ app.use(express.static(__dirname + '/public'));
 
 
 app.use("/Img", express.static(__dirname + "/Img"));
-
+app.use("/style", express.static(__dirname + "/style"));
+app.use("/particles", express.static(__dirname + "/particles"));
 
 app.get("/", function(req,res){
     res.sendFile(__dirname + "/index.html");
